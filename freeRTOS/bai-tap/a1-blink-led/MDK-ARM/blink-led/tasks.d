eb2a6e5f811346d263e34835346967090d5c567a
ducdat7106 <nhiennhienbn@gmail.com>
2022-06-26 17:31:07 +0700

    hc05 bluetooth

--- freeRTOS/bai-tap/a1-blink-led/MDK-ARM/blink-led/tasks.d
+++
@@ -1,3 +0,0 @@
-blink-led\tasks.o: ..\FreeRTOS\Source\tasks.c
-blink-led\tasks.o: E:\installAplication\KeilC\Core\ARM\ARMCC\Bin\..\include\stdlib.h
-blink-led\tasks.o: E:\installAplication\KeilC\Core\ARM\ARMCC\Bin\..\include\string.h
