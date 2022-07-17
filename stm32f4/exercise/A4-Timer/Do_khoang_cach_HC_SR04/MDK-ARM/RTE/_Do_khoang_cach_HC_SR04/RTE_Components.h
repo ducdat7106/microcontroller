23369b459055df77e01541567bc6041530afedfc
ducdat7106 <nhiennhienbn@gmail.com>
2022-07-17 16:04:39 +0700

    add

---
+++ stm32f4/exercise/A4-Timer/Do_khoang_cach_HC_SR04/MDK-ARM/RTE/_Do_khoang_cach_HC_SR04/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Do_khoang_cach_HC_SR04' 
+ * Target:  'Do_khoang_cach_HC_SR04' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
