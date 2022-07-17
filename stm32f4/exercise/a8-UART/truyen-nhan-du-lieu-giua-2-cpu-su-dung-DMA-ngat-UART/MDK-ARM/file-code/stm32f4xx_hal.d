2a9eba6a91613dd0dc0ab571a4eea55d06fa5361
ducdat7106 <nhiennhienbn@gmail.com>
2022-07-17 16:28:27 +0700

    stm32 error

---
+++ stm32f4/exercise/a8-UART/truyen-nhan-du-lieu-giua-2-cpu-su-dung-DMA-ngat-UART/MDK-ARM/file-code/stm32f4xx_hal.d
@@ -0,0 +1,30 @@
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: ../Core/Inc/stm32f4xx_hal_conf.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/CMSIS/Include/core_cm4.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/CMSIS/Include/cmsis_version.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/CMSIS/Include/cmsis_compiler.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/CMSIS/Include/cmsis_armcc.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/CMSIS/Include/mpu_armv7.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+truyen nhan du lieu giua 2 cpu su dung dma (ngat uart)\stm32f4xx_hal.o: C:/Users/Nguyen Duc Dat/STM32Cube/Repository/STM32Cube_FW_F4_V1.26.2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
